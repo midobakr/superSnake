74252af11fdfc0c6e34ee5e1dabc16f520129b2a
bakr <you@example.com>
2021-01-12 23:07:33 +0200

    initial commit

---
+++ precache-manifest.f7081b42236c78e05b51ec18f801ffa8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75a5d9204d1e730cd106b9af0f419a5",
+    "url": "/superSnake/index.html"
+  },
+  {
+    "revision": "707dcb6cf0e3e01f49b7",
+    "url": "/superSnake/static/css/main.3fac8bbb.chunk.css"
+  },
+  {
+    "revision": "5f511e9150daab9742a9",
+    "url": "/superSnake/static/js/2.7dde0347.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/superSnake/static/js/2.7dde0347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707dcb6cf0e3e01f49b7",
+    "url": "/superSnake/static/js/main.1b3fed1d.chunk.js"
+  },
+  {
+    "revision": "8916544b51a6188741ad",
+    "url": "/superSnake/static/js/runtime-main.7da24656.js"
+  },
+  {
+    "revision": "569e8718e1d1244cee61ab3dcd7f229d",
+    "url": "/superSnake/static/media/home.569e8718.svg"
+  },
+  {
+    "revision": "7e19eab305b630026be0f8f561a01730",
+    "url": "/superSnake/static/media/settings.7e19eab3.svg"
+  }
+]);
